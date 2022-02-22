0ac85275f10ac195bd9d208d79d5d400e7d9783f
ahmad khawaga <ahmad.elkhwaga2@gmail.com>
2022-02-22 02:19:42 +0200

    Initial commit

---
+++ TestPodFile/TestPodFile.h
@@ -0,0 +1,18 @@
+//
+//  TestPodFile.h
+//  TestPodFile
+//
+//  Created by A.Khwaja on 22/02/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestPodFile.
+FOUNDATION_EXPORT double TestPodFileVersionNumber;
+
+//! Project version string for TestPodFile.
+FOUNDATION_EXPORT const unsigned char TestPodFileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestPodFile/PublicHeader.h>
+
+
